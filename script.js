5e614b48ad0eeffb45a393edc118621db5a89748
Darin <darinchengg@gmail.com>
2022-05-14 17:08:04 -0500

    Add "rainbow mode" toggle that generates random colors upon hover

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 // NodeList that selects all div containers 
 // for node of NodeList... loop, create and append 16 divs per node
 // Create 16x16 grid of square divs and append to containers
+let rainbow = false;
 const gridContainer = document.querySelector(".grid-container");
 const resizeButton = document.querySelector(".resize-button");
+const rainbowButton = document.querySelector(".rainbow-button");
 const clearButton= document.querySelector(".clear-button");
 resizeButton.addEventListener("click", sizePrompt);
 clearButton.addEventListener("click", clearGrid);
+rainbowButton.addEventListener("click", () => rainbow = !rainbow);
 
 function sizePrompt() {
     const size = parseInt(prompt("Please enter desired number of rows or columns up to a maximum of 100", "16"));
@@ -37,11 +40,14 @@ function resizeGrid(size=16) {
 
 function hoverChange(e) {
     //TODO: change toggle to add, then add clear button option
-    //enable rainbow mode option? create another class with random RGB values that progressively darken
     // if-else statement, if class toggled then darken, if not generate random numbers 
     // if-else to check if colored option is toggled
     const tempDiv = e.target;
-    tempDiv.style["background-color"] = "rgb(0, 0, 0)";
+    if (rainbow) {
+        tempDiv.style["background-color"] = `rgb(${Math.floor(Math.random() * 255 + 1)}, ${Math.floor(Math.random() * 255 + 1)}, ${Math.floor(Math.random() * 255 + 1)})`;
+    } else {
+        tempDiv.style["background-color"] = "black";
+    }
 }
 
 function clearGrid() {
